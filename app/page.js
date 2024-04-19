e0cc3c1f3fe373990e6d7107d3e011228248555b
lcarrio <44201725+lcarrio@users.noreply.github.com>
2024-04-19 18:38:49 -0400

    Added Hero Section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
-
-
+import Mouse from "../public/mouse.png";
 export default function Home() {
   return (
-  <div class="grid h-20 card bg-base-300 rounded-box place-items-center">Game2Mouse</div>
+    <div class="flex items-center justify-between bg-base-300 mx-10 my-2 rounded-box ">
+   <btn class="btn btn-square btn-ghost  mx-2 my-2">
+      <Image src={Mouse} alt="Mouse" />
+     </btn>
     
+    <h1 class="font-bold font-sans text-xl ">Game2Mouse</h1>
+    <h1></h1>
+    </div>
   );
 }
