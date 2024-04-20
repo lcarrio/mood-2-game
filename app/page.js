a2b78eba2ba8d39abac7e21d4488cbb8217ac2b2
lcarrio <44201725+lcarrio@users.noreply.github.com>
2024-04-20 00:16:41 -0400

    Added Game Buttons

--- app/page.js
+++ app/page.js
@@ -1,14 +1,40 @@
 import Image from "next/image";
 import Mouse from "../public/mouse.png";
+import Rifle from "../public/rifle.svg";
+import Pistol from "../public/pistol.svg";
+import Ar from "../public/assault-rifle.svg";
 export default function Home() {
   return (
-    <div class="flex items-center justify-between bg-base-300 mx-10 my-2 rounded-box ">
-   <btn class="btn btn-square btn-ghost  mx-2 my-2">
+    // Navbar with home button 
+    <div>
+    <div class="flex items-center justify-between bg-base-300 mx-20 my-2 rounded-box ">
+   <a class="btn btn-square btn-ghost  mx-2 my-2" href="/">
       <Image src={Mouse} alt="Mouse" />
-     </btn>
-    
+     </a>
     <h1 class="font-bold font-sans text-xl ">Game2Mouse</h1>
-    <h1></h1>
+    <h1 className="mx-4"></h1>
+    
+    </div>
+    <div className="hero">
+  <div className="hero-content text-center">
+    <div className="">
+      <h1 className="text-4xl font-bold font-sans">Discover top-rated mouse based on your game</h1>
+      <p className="py-6">Which game are you playing now?</p>
+    </div>
+  </div>
+</div>
+
+<div className="grid grid-cols-3 grid-rows-2 gap-4 justify-items px-20">
+  <a class="btn btn-outline btn-primary  " href="/">
+    <h1 class="flex text-2xl font-bold"> <Image src={Rifle} alt="CsIcon" class="h-8 w-8" />CS:GO </h1>
+  </a>
+  <a class="btn btn-outline btn-primary  " href="/">
+    <h1 class="flex text-2xl font-bold"> <Image src={Pistol} alt="ValIcon" class="h-8 w-8" />Valorant</h1>
+  </a>
+  <a class="btn btn-outline btn-primary  " href="/">
+    <h1 class="flex text-2xl font-bold"> <Image src={Ar} alt="ApexIcon" class="h-8 w-8" />Apex</h1>
+  </a>
+</div>
     </div>
   );
 }
