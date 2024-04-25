2f0722f2856e7c4247880cb2b15a697803fcb281
lcarrio <44201725+lcarrio@users.noreply.github.com>
2024-04-25 02:02:31 -0400

    optimized seo compatibility

--- components/footer.js
+++ components/footer.js
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer ckass="footer" className="footer footer-center p-4 bg-base-300 text-base-content">
+        <footer ckass="footer" className="footer footer-center bg-base-300 text-base-content">
   <aside>
     <p class="text-xl">Made for fun by Laz 🚀</p>
   </aside>
