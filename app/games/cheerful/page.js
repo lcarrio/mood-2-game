b2f3fbe73bb7895adefd4f4ec22b2a23f0eb2046
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-04-29 19:07:15 -0400

    added mongoDB dataset

--- app/games/cheerful/page.js
+++ app/games/cheerful/page.js
@@ -3,9 +3,16 @@ import MouseCard from "@/components/gamecard";
 import Footer from "@/components/footer";
 import Header from "@/components/navbar";
 import GameCard from "@/components/gamecard";
+import Navbar from "@/components/navbar";
 export default function Mouse() {
     return(
-      <GameCard/>
-      
+    <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
+      <main>
+          <Navbar NavIcon={"/logo2.png"}NavText={"Feeling 😁 Cheerful"} />
+          <div class="card card-compact md:card-normal w-full md:w-[40rem] bg-base-100 shadow-xl mx-auto">
+          
+          </div>
+      </main>
+    </div>
 );
 }
