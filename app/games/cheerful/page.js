a1cbcc27a7402273fd12c25d613f7f1250df8af8
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-05-28 12:24:04 -0400

    Added Api Fetching

--- app/games/cheerful/page.js
+++ app/games/cheerful/page.js
@@ -1,4 +1,10 @@
 import Navbar from "../../../components/navbar";
+
+async function getCheerful() {
+  const res = await fetch("https://api.mood2game.com/cheerful");
+  const data = await res.json();
+  return data;
+}
 export default function Cheerful() {
   return (
     <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
