cae368113410486ce2926a62039abc615a3756ff
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-05-31 12:31:55 -0400

    updated pages

--- app/games/cheerful/page.js
+++ app/games/cheerful/page.js
@@ -1,5 +1,5 @@
 import Navbar from "../../../components/navbar";
-
+import { Query } from "../api/db";
 async function getCheerful() {
   const res = await fetch("https://api.mood2game.com/cheerful");
   const data = await res.json();
@@ -9,8 +9,10 @@ export default function Cheerful() {
   return (
     <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
       <main>
-        <Navbar NavIcon={"/logo2.png"} NavText={"Feeling 😁 Cheerful"} />
-        <div class="card card-compact md:card-normal w-full md:w-[40rem] bg-base-100 shadow-xl mx-auto"></div>
+        <Navbar NavIcon={"/logo2.png"} NavText={"Feeling 😁 Cheerful "} />
+        <div class="card card-compact md:card-normal w-full md:w-[40rem] bg-base-100 shadow-xl mx-auto">
+          {Query().then((game) => game.name)}
+        </div>
       </main>
     </div>
   );
--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 import { Query } from "./api/db";
 
-let gameID = 1;
-Query().then((game) => console.log(game.appid));
 export default async function Home() {
   return (
     <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
