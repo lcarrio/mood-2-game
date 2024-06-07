2761f0c5f1484d7c1bb44d3e843c9b8057c5faa3
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-06-07 18:02:07 -0400

    tested api

--- app/games/cheerful/page.js
+++ app/games/cheerful/page.js
@@ -3,14 +3,18 @@ import { Query } from "../../api/db";
 
 export default function Cheerful() {
   const QueryGenre = "Indie;Strategy";
-  //const gameName = Query(QueryGenre).then((game) => game.name);
-  console.log(Query(QueryGenre));
+  //const gameName = Query(QueryGenre).then((game) => game.name + " " + game._id);
+  var gameName;
+  Query(QueryGenre).then((game) => {
+    gameName = game.name + " " + game._id;
+  });
+
   return (
     <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
       <main>
         <Navbar NavIcon={"/logo2.png"} NavText={"Feeling 😁 Cheerful "} />
         <div class="card card-compact md:card-normal w-full md:w-[40rem] bg-base-100 shadow-xl mx-auto">
-          {Query(QueryGenre).then((game) => game.name)}
+          {gameName}
         </div>
       </main>
     </div>
