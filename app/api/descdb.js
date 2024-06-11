f4ec4f7e9ae1f77e644f2b14dede341accfb803c
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-06-11 18:20:24 -0400

    Added appid queries

--- app/api/descdb.js
+++ app/api/descdb.js
@@ -3,18 +3,22 @@ const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 
-export async function DescQuery(appID) {
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+export async function QueryAppid(appid) {
   try {
-    if (appID === undefined) {
+    if (QueryAppid === undefined) {
       return "Query is undefined!";
     }
     const database = client.db("mood2game");
     const games = database.collection("steam_description");
     // Query for a game
-    const query = { steam_appid: appID };
-    const description = await games.find(query);
+    const query = { steam_appid: appid };
+    const game = await games.findOne(query);
+    console.log(game);
     //Return a game with
-    return description;
+    return game;
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
--- app/games/cheerful/page.js
+++ app/games/cheerful/page.js
@@ -1,8 +1,10 @@
 import Navbar from "../../../components/navbar";
 import { Query } from "../../api/steamdb";
-import { DescQuery } from "../../api/descdb";
+import { QueryAppid } from "../../api/descdb";
 const QueryGenre = "Indie;Strategy";
 const game = await Query(QueryGenre);
+const gameDesc = await QueryAppid(game.appid);
+console.log(gameDesc);
 export default function Cheerful() {
   return (
     <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
@@ -10,6 +12,7 @@ export default function Cheerful() {
         <Navbar NavIcon={"/logo2.png"} NavText={"Feeling 😁 Cheerful "} />
         <div class="card card-compact md:card-normal w-full md:w-[40rem] bg-base-100 shadow-xl mx-auto">
           {game.name}
+          <div>{gameDesc.short_description}</div>
         </div>
       </main>
     </div>
