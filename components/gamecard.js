6fda0d19728692f368964eb3cfa0a172b369e4b7
Lazaro Carrio <44201725+lcarrio@users.noreply.github.com>
2024-07-01 19:30:03 -0400

    Update gamecard.js

--- components/gamecard.js
+++ components/gamecard.js
@@ -2,7 +2,7 @@ import Navbar from "./navbar";
 
 export default function GameCard() {
   return (
-    <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto text-bold">
+    <div class="min-h-screen w-full p-2 lg:p-6 mb-20 md:mb-24 max-w-4xl mx-auto">
       <main>
         <Navbar NavIcon={"/logo2.png"} NavText={"Feeling 😁 Cheerful"} />
       </main>
